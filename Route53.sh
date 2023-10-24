cb30277bdd0808685508ce9f4984eaedc2782ba8
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-10-24 18:56:30 +0530

    Adding New Files

---
+++ Route53.sh
@@ -0,0 +1,5 @@
+#Route53 Concepts:
+
+Route53 is DNS as a Service 
+DNS --> Domain Name System
+    Domain Name System, translates human readable domain names (for example, www.amazon.com) to machine readable IP addresses (for example, 192.0.2.44).
---
+++ VPC.sh
@@ -0,0 +1,26 @@
+# VPC --> Virtual Private Cloud
+
+What and Why VPC Is required ?
+    VPC is required to secure your virtual networking environment Including IP Address,Subnets,Network Gateways
+    Clients can run code ,store data,host websites and do all the things what they can do in private cloud
+Inside VPC How data transfers?
+    Inside VPC TYhe whole data is transfered with the connection of Private Ip Address
+
+Components in VPC?
+    IP Address Ranges
+    Internet Gateway
+    Public Subnet
+    Private Subnet
+    Load Balancer
+    Route Table (Routers)
+    Security Groups
+    NACL --> Automation for Security Group
+    NAT Gateway --> Network Address Translation
+    Domain Name Systems (DNS)
+    Subnet Masking
+    Nat is helping to download the resource/package from internet and masking the ip address for which server these resourecs are downloading
+    VPC Flowlogs --> record every action in VPC acting inside .
+
+# Security Groups and NACL 
+
+
